ae30cfeb81720596aae267e25e0da24768e2b2bf
Nac Abarca <nacchile@gmail.com>
2018-07-02 16:23:12 -0400

    Agregar contenidos para multimedia y Archivo (Temporal)

---
+++ wp-content/themes/wp-bootstrap-starter/template-multimedia/content.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Template part for displaying posts
+ *  // TODO: Este contenido para multimedia son Videoteca, Exposición y Musica de la Época
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WP_Bootstrap_Starter
+ */
+
+?>
+
+
+
+<!-- start: card -->
+<div id="post-<?php the_ID(); ?>" class="card">
+	<!-- start: imagen -->
+  <?php if (has_post_thumbnail()) {
+  	the_post_thumbnail();
+  }?>
+	<!-- end-imagen -->
+	<!-- start: card-body -->
+  <div class="card-body">
+		<?php
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . esc_html__( 'Páginas:', 'wp-bootstrap-starter' ),
+				'after'  => '</div>',
+			) );
+		?>
+		<!-- start: card-title -->
+		<?php
+		if ( is_single() ) :
+			the_title( '<h5 class="card-title">', '</h5>' );
+		else :
+			the_title( '<h5 class="card-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h5>' );
+		endif;
+		?>
+		<!-- end: card-title -->
+		<!-- start: card-text -->
+    <p class="card-text">
+			<?php
+			if ( is_single() ) :
+				the_content();
+			else :
+				the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'wp-bootstrap-starter' ) );
+			endif;
+			?>
+		</p>
+		<!-- end: card-text -->
+  </div>
+	<!-- end: card-body -->
+	<!-- start: card-footer -->
+	<div class="card-footer">
+		<div class="text-muted">
+			<?php wp_bootstrap_starter_posted_on();?>
+		</div>
+		<?php wp_bootstrap_starter_entry_footer(); ?>
+
+	</div>
+	<!-- end: card-footer -->
+</div>
+<!-- start: card -->
+<a class="sr-only sr-only-focusable" href="#content">Skip to main content</a>
+<br/>
